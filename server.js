4db510e75ac37849234336ff835d77cfb16ed888
todddrinkwater <daniel-reason>
2017-04-04 11:17:09 +1200

    input all pw code. server not working

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-var path = require('path')
+const express = require('express')
+const flash = require('connect-flash')
+const hbs = require('express-handlebars')
+const LocalStrategy = require('passport-local')
+const passport = require('passport')
+const path = require('path')
 
-var express = require('express')
+const auth = require('./lib/auth')
+const users = require('./lib/users')
+const session = require('./lib/session')
+
+const indexRoutes = require('./routes')
+
+
+// var path = require('path')
+// var express = require('express')
 var bodyParser = require('body-parser')
-var hbs = require('express-handlebars')
+// var hbs = require('express-handlebars')
 
 var index = require('./routes/index')
 
 var server = express()
 
-module.exports = server
+passport.use(new LocalStrategy(auth.verify))
+passport.serializeUser(users.serialize)
+passport.deserializeUser(users.deserialize)
+
 
 // Middleware
 
@@ -18,9 +34,16 @@ server.set('views', path.join(__dirname, 'views'))
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(express.static('public'))
 
+server.use(session)
+server.use(flash())
+server.use(passport.initialize())
+server.use(passport.session())
+server.use('/', indexRoutes)
 // Routes
 
 server.get('/dogs', index.allDogs)
 server.get('/find-a-dog', index.match)
 
 // server.get('profile/:id'*******)
+
+module.exports = server
